e050c5bf4f4eeead03d39ae14bb91aa90ffb36d4
Samil BOYALIKLI <esocraton@gmail.com>
2024-05-21 23:20:43 +0300

    dayPlanner.js
    - fixed variable bug
    - completed sketch of times
    - added some CSS codes

--- dayPlanner.js
+++ dayPlanner.js
@@ -1,94 +1,97 @@
+var element = document.getElementById("settingBox");
+var addTodo = document.getElementById("addTodo");
+var todoText = document.getElementById("todoArea");
+var close = document.getElementById("close");
+
+var funcGroup = {};
+var func = "func";
+var activeFuncNum = null;
+
+for (var i = 1; i <= 15; i++) {
+    funcGroup[func + i] = (function(num) {
+        return function() {
+            activeFuncNum = num;
+            if (element.style.display === "none" || element.style.display === "") {
+                element.style.display = "block";
+            } else {element.style.display = "none";}
+        };
+    })(i);
+}
+
+addTodo.addEventListener("click", function() {
+    if (activeFuncNum !== null) {
+        let td = "td" + activeFuncNum;
+        var todoItself = todoText.value;
+        var tdText = document.getElementById(td)
+        tdText.innerText = todoItself
+        console.log(todoItself);
+        todoText.value = ""
+    } else {console.log("No active function");}
+});
+
+
+close.addEventListener("click", function() {element.style.display = "none";});
+
+
+
+/*
 var element = document.getElementById("settingBox")
+var addTodo = document.getElementById("addTodo")
 var close = document.getElementById("close")
+var todoText = document.getElementById("todoArea")
 
-function clickableTR1() {
-        if (element.style.display === "none" || element.style.display === "") 
-            {element.style.display = "block"}
-        else {element.style.display = "none"}
-}  
-
-function clickableTR2() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR3() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR4() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR5() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR6() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR7() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR8() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR9() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR10() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR11() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR12() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR13() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR14() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
-
-function clickableTR15() {
-    if (element.style.display === "none" || element.style.display === "") 
-        {element.style.display = "block"}
-    else {element.style.display = "none"}
-}
 
 close.addEventListener("click", function() {element.style.display = "none"})
+
+var funcGroup = {}
+var func = "func"
+for (var i = 1; i <= 15; i++) {
+    funcGroup[func + i] = (function(num){
+        return function() {
+            if (element.style.display === "none" || element.style.display === "") 
+                {element.style.display = "block";
+                addTodo.addEventListener("click", function() {
+                    var td = "td" + num
+                    console.log(td)
+                });
+            } else {element.style.display = "none"}
+        }})(i)
+}
+*/
+
+/*
+BURADA BİR PROBLEM YOK
+var funcGroup = {}
+var func = "func"
+for (var i = 1; i <= 15; i++) {
+    funcGroup[func + i] = (function(num){
+        return function() {
+            if (element.style.display === "none" || element.style.display === "") 
+                {element.style.display = "block";
+                addTodo.addEventListener("click", function() {
+                    var td = "td" + num
+                    console.log(td)
+                });
+            }
+            else {element.style.display = "none"}
+        }})(i)
+}
+
+---- AMA EĞER TD'DEN SONRA NUM DEĞİL DE İ KULLANIRSAK İ'NİN DEĞERİ 16 OLUYOR
+
+var funcGroup = {}
+var func = "func"
+for (var i = 1; i <= 15; i++) {
+    funcGroup[func + i] = (function(num){
+        return function() {
+            if (element.style.display === "none" || element.style.display === "") 
+                {element.style.display = "block";
+                addTodo.addEventListener("click", function() {
+                    var td = "td" + i
+                    console.log(td)
+                });
+            }
+            else {element.style.display = "none"}
+        }})(i)
+}
+*/
